c97954f9d302c96a25d9ef02adf77b31cf5b2cab
Ilya Kantor <iliakan@gmail.com>
2015-02-17 21:02:02 +0300

    improvements

--- src/PdfBookPolisher.java
+++ src/PdfBookPolisher.java
@@ -1,11 +1,18 @@
 
 
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
+import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.FileOptionHandler;
 
 
 /**
@@ -14,13 +21,31 @@ import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlin
  *
  * @author Ilya Kantor
  */
-public class PdfMakeOutline {
+public class PdfBookPolisher {
     /**
      * Constructor.
      */
-    private PdfMakeOutline() {
+    private PdfBookPolisher() {
         //utility class
     }
+
+    @Option(name="--widthTolerance",usage="Sets a width tolerance to left/right sides of the links for text extraction")
+    public int widthTolerance = 10;
+
+    @Option(name="--input", handler=FileOptionHandler.class, usage="Input pdf-file")
+    public File inputFile;
+
+    @Option(name="--output", handler=FileOptionHandler.class, usage="Output pdf-file")
+    public File outputFile;
+
+    @Option(name="--cover", handler=FileOptionHandler.class, usage="Cover pdf-file")
+    public File coverFile = null;
+
+    @Option(name="--pageFrom",usage="The first page to extract ToC")
+    public int pageFrom;
+
+    @Option(name="--pageTo",usage="The last page to extract ToC")
+    public int pageTo;
 
     /**
      * This will read in a document and replace all of the urls with
@@ -33,23 +58,38 @@ public class PdfMakeOutline {
      */
     public static void main(String[] args) throws Exception {
 
-        if (args.length != 4) {
-            usage();
-            return;
+        PdfBookPolisher polisher = new PdfBookPolisher();
+
+        CmdLineParser parser = new CmdLineParser(polisher);
+
+
+        try {
+            parser.parseArgument(args);
+            polisher.run();
+        } catch (CmdLineException e) {
+            // handling of wrong arguments
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
         }
 
-        int widthTolerance = 10;
+    }
 
-        PDDocument doc = PDDocument.load(new File(args[0]));
+    protected void run() throws IOException, COSVisitorException {
+
+        PDDocument doc = PDDocument.load(inputFile);
+
+        if (coverFile != null) {
+            PDDocument cover = PDDocument.load(coverFile);
+            doc.getDocumentCatalog().getPages().getKids().add(0, (PDPage) cover.getDocumentCatalog().getAllPages().get(0));
+        }
 
         ArrayList<LinkInfo> linkInfoList = new ArrayList<LinkInfo>();
 
         LinkInfoExtractor extractor = new LinkInfoExtractor(doc, widthTolerance);
 
-        for (int pageNum = Integer.parseInt(args[3]); pageNum < Integer.parseInt(args[4]); pageNum++) {
+        for (int pageNum = pageFrom; pageNum <= pageTo; pageNum++) {
             linkInfoList.addAll( extractor.extractPage(pageNum) );
         }
-
 
         PDDocumentOutline outline = new PDDocumentOutline();
         doc.getDocumentCatalog().setDocumentOutline(outline);
@@ -59,7 +99,7 @@ public class PdfMakeOutline {
 
             LinkInfo linkInfo = linkInfoList.get(i);
 
-            // add 2nd level
+            // add 2nd level of bookmarks
             while (linkInfo.getRectangle().getX() > firstLevelX) {
                 PDOutlineItem outlineItem = new PDOutlineItem();
                 outlineItem.setTitle(linkInfo.getText());
@@ -80,15 +120,8 @@ public class PdfMakeOutline {
             outline.appendChild(outlineItem);
         }
 
-
-        doc.save(args[1]);
-
+        doc.save(outputFile);
+        System.out.println("Done.");
     }
 
-    /**
-     * This will print out a message telling how to use this example.
-     */
-    private static void usage() {
-        System.err.println("usage: " + PdfMakeOutline.class.getName() + " <input-file> <output-file> <page-from> <page-to>");
-    }
 }
